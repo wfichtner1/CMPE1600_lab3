83e4464e73ee98189d9c45b30dcf6915ff809b6e
William Fichtner <william_25_35@hotmail.com>
2017-02-26 14:13:24 -0700

    oh boy

--- CMPE1600_Lab3/Form1.cs
+++ CMPE1600_Lab3/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using GDIDrawer;
 
 namespace CMPE1600_Lab3
 {
+    [Serializable] public struct Circle
+    {
+        public int _diameter;
+        public Point _center;
+        public Color _color;
+                
+       public Circle(int diameter, Color color, Point point)
+        {
+            _diameter = diameter;
+            _color = color;
+            _center = point;
+        }
+    }
+
     public partial class Form1 : Form
     {
+        List<Circle> circList = new List<Circle>();
         public Form1()
         {
             InitializeComponent();
+            CDrawer canvas = new CDrawer();
+        }
+
+       
+
+        private void UI_DrawButton_Click(object sender, EventArgs e)
+        {
+            DrawTimer.Enabled = true;
+
+        }
+
+        private void UI_OpenFile_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
